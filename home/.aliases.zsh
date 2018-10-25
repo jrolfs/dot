1c9b50b12e9ab88650bc8c1177ba5493590d5480
Jamie Rolfs <jamie.rolfs@gmail.com>
2018-10-25 10:21:22 -0700

    Add a couple functions that were in my macOS dotfiles but are actually agnostic

--- home/.aliases.zsh
+++ home/.aliases.zsh
@@ -1,31 +1,44 @@
-# GnuPG
-alias gty="gpg-connect-agent updatestartuptty /bye"
 
-# Editors
-alias vi=nvim
-alias vim=nvim
-
-# homeshick
-alias hs=homeshick
-
-# fasd
-alias jj="fasd_cd -tdi"
-
-# Git
-alias gl1="git log --pretty=oneline"
-
+# Helper to check existence of a command
 function exists {
   return $(command -v $1 >&/dev/null 2>&1)
 }
+
+
+#
+# Aliases
+
+alias gty="gpg-connect-agent updatestartuptty /bye"
+
+alias vi=nvim
+alias vim=nvim
+
+alias hs=homeshick
+
+alias jj="fasd_cd -tdi"
+
+alias gl1="git log --pretty=oneline"
 
 if exists 'code-insiders'; then
   exists 'code' && alias code-stable=code
   alias code=code-insiders
 fi
 
-# tmux
+
+#
+# Functions
+
 function mxd {
   tmux rename-window "$(basename $(pwd))"
+}
+
+
+function nix-search {
+  nix-env -qa ".*$1.*"
+}
+
+function nix-rip {
+  rg -g "**/*$1*/**/default.nix" --files --hidden ~/.nix-defexpr/nixpkgs/pkgs
 }
 
 function lpwd {
@@ -33,4 +46,5 @@ function lpwd {
 }
 
 
+# Clean up `exists` helper
 unset -f exists # ¯\_(ツ)_/¯
