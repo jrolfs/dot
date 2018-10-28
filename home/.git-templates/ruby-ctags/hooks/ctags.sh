b6445aa54c8d6bc6797d8ff0070e303d8f88da04
Jamie Rolfs <jamie.rolfs@gmail.com>
2018-10-27 19:52:21 -0700

    Remove old Ruby CTAGS hooks Git template

--- home/.git-templates/ruby-ctags/hooks/ctags.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-PATH="/usr/local/bin:$PATH"
-dir="`git rev-parse --git-dir`"
-trap 'rm -f "$dir/$$.tags"' EXIT
-ctags -R -f"$dir/$$.tags" --languages=ruby --exclude=.git --exclude=log . $(bundle list --paths) 
-mv "$dir/$$.tags" "$dir/../tags"
