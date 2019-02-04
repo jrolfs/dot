d4477cff8a27b7d92116ccc66a38fddfb0228cc8
Jamie Rolfs <jamie.rolfs@gmail.com>
2019-02-04 01:29:31 -0800

    firefox: persisted ui... er toolbar? I forgot

--- home/Library/Application Support/Firefox/Profiles/jamie/user.js
+++ home/Library/Application Support/Firefox/Profiles/jamie/user.js
@@ -7,3 +7,31 @@ user_pref("general.warnOnAboutConfig", false);
 // Developer
 
 user_pref("devtools.theme", "dark");
+
+//
+// Extensions
+
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
+user_pref("extensions.webextensions.restrictedDomains", "");
+
+//
+// Interface
+
+user_pref("browser.pageActions.persistedActions", `
+  {
+    "version": 1,
+    "ids": [
+      "bookmark",
+      "bookmarkSeparator",
+      "copyURL",
+      "emailLink",
+      "addSearchEngine",
+      "sendToDevice",
+      "shareURL",
+      "pocket",
+      "webcompat-reporter_mozilla_org",
+      "screenshots_mozilla_org"
+    ],
+    "idsInUrlbar": []
+  }
+`);
