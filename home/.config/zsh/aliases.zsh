8dbd8fadd2e104d20f9b6b5b5ffa7973e47b15e1
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-03-13 18:23:38 -0700

    zsh: add alias to do what I assumed 'git remote prune' did
    Found here: 'https://stackoverflow.com/questions/13064613/how-to-prune-local-tracking-branches-that-do-not-exist-on-remote-anymore#17029936'

--- home/.config/zsh/aliases.zsh
+++ home/.config/zsh/aliases.zsh
@@ -10,3 +10,5 @@ alias jj="fasd_cd -tdi"
 alias gl1="git log --pretty=oneline"
 
 alias cz=git-cz
+
+alias gRP="git branch -r | awk '{ print \$1 }' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{ print \$1 }' | xargs git branch -D"
