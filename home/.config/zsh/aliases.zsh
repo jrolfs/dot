8508ca83f724a38d977d153bf5fb67f9e5ec2431
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-09-24 00:46:15 -0700

    zsh: add ls→exa aliases

--- home/.config/zsh/aliases.zsh
+++ home/.config/zsh/aliases.zsh
@@ -12,3 +12,10 @@ alias gl1="git log --pretty=oneline"
 alias cz=git-cz
 
 alias gRP="git branch -r | awk '{ print \$1 }' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{ print \$1 }' | xargs git branch -D"
+
+if command -v exa >&/dev/null 2>&1; then
+  alias l="exa --icons -1a"
+  alias ll="exa --icons -l"
+  alias la="exa --icons -al"
+  alias ls="exa --icons -G"
+fi
