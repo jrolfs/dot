c9f59865e70a5394283a60368e988db788c407ef
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-11-08 09:38:29 -0800

    zsh: set `STARSHIP_ENABLED` environment variable

--- home/.config/zsh/starship.zsh
+++ home/.config/zsh/starship.zsh
@@ -2,8 +2,10 @@
 # super slow, see https://github.com/starship/starship/issues/1617
 if [[ -a "$(pwd)/.git/_starship_disable" ]]; then
   zstyle ':prezto:module:prompt' theme 'pure'
+  unset STARSHIP_ENABLED
 else
-  eval "$(starship init zsh)"
+  zstyle ':prezto:module:prompt' theme 'default'
+  STARSHIP_ENABLED=1
 fi
 
 # Prints new line between prompts
