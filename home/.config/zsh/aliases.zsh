cb934d00a591b8e788ec6ae90c15b26a374e59ad
Jamie Rolfs <jamie.rolfs@gmail.com>
2021-05-28 22:37:08 -0700

    zsh: add wrapper to alias exa to ls

--- home/.config/zsh/aliases.zsh
+++ home/.config/zsh/aliases.zsh
@@ -14,10 +14,7 @@ alias cz=git-cz
 alias gRP="git branch -r | awk '{ print \$1 }' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{ print \$1 }' | xargs git branch -D"
 
 if command -v exa >&/dev/null 2>&1; then
-  alias l="exa --icons -1a"
-  alias ll="exa --icons -l"
-  alias la="exa --icons -al"
-  alias ls="exa --icons -G"
+  alias ls="$XDG_DATA_HOME/exa-wrapper.sh"
 fi
 
 alias icat="kitty +kitten icat"
