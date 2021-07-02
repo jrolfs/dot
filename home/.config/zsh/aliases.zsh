16bf8142ac7f9c7ba3dea59105b1fd69401109f7
Jamie Rolfs <jamie.rolfs@gmail.com>
2021-07-01 22:10:51 -0700

    zsh: organize and remove a couple aliases

--- home/.config/zsh/aliases.zsh
+++ home/.config/zsh/aliases.zsh
@@ -1,22 +1,22 @@
-alias gty="gpg-connect-agent updatestartuptty /bye"
-
 alias vi=nvim
 alias vim=nvim
 
 alias hs=homeshick
 
-alias jj="fasd_cd -tdi"
+# Git
 
 alias gl1="git log --pretty=oneline"
-
-alias cz=git-cz
-
 alias gRP="git branch -r | awk '{ print \$1 }' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{ print \$1 }' | xargs git branch -D"
+
+# Navigation
 
 if command -v exa >&/dev/null 2>&1; then
   alias ls="$XDG_DATA_HOME/exa-wrapper.sh"
 fi
 
-alias icat="kitty +kitten icat"
+alias jj="fasd_cd -tdi"
 
+# Images
+
+alias icat="kitty +kitten icat"
 alias heic2jpg="fd --regex '(?i)heic' -x sips -s format jpeg {/} --out {/.}.jpg"
