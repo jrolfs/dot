b43466cf07d404aa828288a2bd7e153214b4895b
Jamie Rolfs <jamie.rolfs@gmail.com>
2021-09-18 18:23:00 -0700

    zsh(python): make SSL configuration a function

--- home/.config/zsh/python.zsh
+++ home/.config/zsh/python.zsh
@@ -1,9 +1,11 @@
 # Fix Python SSL issue
 # https://github.com/danhper/asdf-python/issues/106#issuecomment-873814320
 
-local certificate_path=$(python -m certifi 2>/dev/null)
+function python-ssl {
+  local certificate_path=$(python -m certifi)
 
-if [ -n "$certificate_path" ]; then
-  export SSL_CERT_FILE=${certificate_path}
-  export REQUESTS_CA_BUNDLE=${certificate_path}
-fi
+  if [ -n "$certificate_path" ]; then
+    export SSL_CERT_FILE=${certificate_path}
+    export REQUESTS_CA_BUNDLE=${certificate_path}
+  fi
+}
