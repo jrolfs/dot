d4a9ff130ca2003b6d6f6b7b25d0079b6b5a6996
Jamie Rolfs <jamie.rolfs@hover.to>
2023-09-09 01:07:17 -0700

    zsh(completion): track most directly for now, not sure why not running

--- home/.config/zsh/completion.zsh
+++ home/.config/zsh/completion.zsh
@@ -2,11 +2,21 @@ function exists {
   return $(command -v $1 >&/dev/null 2>&1)
 }
 
-exists kitty && kitty + complete setup zsh | source /dev/stdin
-exists gh && gh completion -s zsh >! $XDG_CONFIG_HOME/zsh/completions/_gh
+# NOTE: seems like this file isn't running for some reason... zplug? Anyways,
+# I've added most of these directly to the repo. I'm honestly not sure how the
+# Kitty completion is working right now.
 
+# # Kitty
+# exists kitty && kitty + complete setup zsh | source /dev/stdin
 
-# See: https://github.com/mklabs/tabtab
-[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
+# # GitHub
+# exists gh && gh completion -s zsh >! $XDG_CONFIG_HOME/zsh/completions/_gh
+
+# # Infrastructure stuff
+# exists kubectl && kubectl completion zsh >! $XDG_CONFIG_HOME/zsh/completions/_kubectl
+# exists infractl && infractl completion zsh >! $XDG_CONFIG_HOME/zsh/completions/_infractl
+
+# # See: https://github.com/mklabs/tabtab
+# [[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
 
 unset exists
